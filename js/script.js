ace918efbcc40fe600aba6b9d4619093cebb0252
cssdominique <dominique@christiansteven.com>
2016-05-09 17:28:34 -0400

    re-align code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// Requires the user to answer a number ( at least 5) of questions, one at a time
-// Prevents the user from skipping questions
-// Gives some way for the user to answer
-// Compares the user answer, and the correct answer to determine a score
-// When all the questions are answered, display the user score.
-// Allow the user to start a new game.
-// Use JavaScript objects to represent the questions.
 var count;
 var correctAnswer;
 var trivia=[
@@ -44,7 +37,7 @@ var trivia=[
 	 	choices: [
 	 		'Gerald Wallace',
 	 		'Bobby Phills',
-	 		'	Stephen Jackson',
+	 		'Stephen Jackson',
 	 		'Michael Jordan'
 	 	]
 	 },
@@ -69,19 +62,8 @@ $(document).ready(function(){
 	 	newGame();
 	 	userName();
 	 	playGame();
-	 	//checkAnswer(count);
     e.preventDefault();
   })
-
-	// $('#startButton').on('click', function(){
-	//  	$('#intro').fadeOut(1000);
-	//  	userName();
-	//  	newGame();
-	//  });
-
-
-	
-// clue : jquery's $.each method
 
 //Get user name
 function userName(){
@@ -92,9 +74,6 @@ function userName(){
 
  // checkAnswer function
  function checkAnswer(questionNumber){
-  // 	jQuery.each( trivia[questionNumber].choices, function( i, val ) {
-  // 		$(".answer").append("<li>" + val + "</li>");
-  // console.log("Choices " + val + "." );})
    if ($(this).text()==trivia[questionNumber].answer) {
    	$("span.circle").text("2/5");
    	correctAnswer++;
@@ -104,11 +83,6 @@ function userName(){
   	$("span.circle").text("2/5");
   	console.log(correctAnswer);
    }
-
-  // $(".triviaQuestion").text(trivia[1].question);
-  //  jQuery.each( trivia[1].choices, function( i, val ) {
-  // 		$(".answer").append("<li>" + val + "</li>")
-  // console.log("Choices " + val + "." );})
  }
 
  //Start New Game
@@ -118,50 +92,15 @@ function userName(){
  questionNumber=2;
  $("#userName").text("");
  $("#result").css("display","none");
-
- 	//User enters name
- //Question
- 	//Display first question and answer choices
- // 	$(".triviaQuestion").text(trivia[count].question);
- // 	//$('.answer li').text(trivia[0].choices[0]);
- // 	jQuery.each( trivia[count].choices, function( i, val ) {
- // 		$(".answer").append("<li>" + val + "</li>")
- // console.log("Choices " + val + "." );})
-
- // 	$( ".answer li" ).click(function() {
- //  $( ".answer li" ).slideUp();
- //  console.log( $(this).text());
- //  //CheckAnswer
- // if ($(this).text()==trivia[count].answer) {
- //   	$("span.circle").text("2/5");
- //   	correctAnswer++;
- //   	console.log("User has " + correctAnswer +"Correct Answers");
- //   }
- //   else{
- //  	$("span.circle").text("2/5");
- //  	console.log("User has " + correctAnswer +"Correct Answers");
- //   }
-
-  // $(".triviaQuestion").text(trivia[1].question);
-  //  jQuery.each( trivia[1].choices, function( i, val ) {
-  // 		$(".answer").append("<li>" + val + "</li>")
-  // console.log("Choices " + val + "." );})
- 
-
-//});
- 	//Create Var to track correct/incorrect answers
- 	//var 
- 	//User selects answer
  	return count,correctAnswer, questionNumber;
  }
 
  //PlayGame
  function playGame(){
  	$(".triviaQuestion").text(trivia[count].question);
- 	//$('.answer li').text(trivia[0].choices[0]);
  	jQuery.each( trivia[count].choices, function( i, val ) {
- 		$(".answer").append("<li>" + val + "</li>")
- console.log("Choices " + val + "." );})
+ 	$(".answer").append("<li>" + val + "</li>")
+  console.log("Choices " + val + "." );})
 
  	$( ".answer li" ).click(function() {
   $( ".answer li" ).slideUp();
@@ -188,7 +127,6 @@ function userName(){
    	$(".triviaQuestion").css("background-color","red");
    	$("#result").text("Correct Answers: "+correctAnswer+"/5");
    	$("#result").css("display","block");
-   	
    }
    count++;
    questionNumber++;
@@ -203,23 +141,14 @@ function userName(){
    		$("#newGameButton").removeAttr('style');
    		$(".answer li").remove();
    		newGame();
-
    	});
    }
    else {setTimeout(function(){
-    //do what you need here
     $(".triviaQuestion").removeAttr('style');
     playGame();
 		}, 2500);
  		}
    
  });
- }
-
-
- //Display User score & calculate user score
- function userScore(){}
-
- function incrementQuestionNum(){
  }
 });
